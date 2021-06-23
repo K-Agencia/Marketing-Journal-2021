2e5c981968a3796e19724d84b2365e884dbea244
Daniel Cosme <pipecosme123@gmail.com>
2021-06-23 17:06:54 -0500

    Se añadieron nuevas fotos

--- src/Constant/Images.js
+++ src/Constant/Images.js
@@ -7,17 +7,21 @@ import Implementaciones from "../img/Implementaciones.svg";
 import Flecha from "../img/right-arrow.svg";
 
 // -------------------------------------------------
-import KVZigzag_MKTJ from "../img/MKT  JOURNAL/KV Zigzag _ MKTJ.jpg";
-import KVAxion_MKTJ from "../img/MKT  JOURNAL/KV Axion_ MKTJ.jpg";
-import KVHC_MKTJ from "../img/MKT  JOURNAL/KV HC_ MKTJ.jpg";
+import KVZigzag_MKTJ from "../img/MKT  JOURNAL/ZigZag/KV Zigzag _ MKTJ.jpg";
+import KVAxion_MKTJ from "../img/MKT  JOURNAL/Axion/KV Axion_ MKTJ.jpg";
+import KVHC_MKTJ from "../img/MKT  JOURNAL/CuidaPlaneta/KV HC_ MKTJ.jpg";
 import KVPtxjabón_MKTJ from "../img/MKT  JOURNAL/KV Ptx jabón_ MKTJ.jpg";
 import KVPtx_MKTJ from "../img/MKT  JOURNAL/KV Ptx_ MKTJ.jpg";
 import KVSS_MKTJ from "../img/MKT  JOURNAL/KV SS_ MKTJ.jpg";
-import KVSuavitel_MKTJ from "../img/MKT  JOURNAL/KV Suavitel_ MKTJ.jpg";
-import KVTotal12_MKTJ from "../img/MKT  JOURNAL/KV Total12 _ MKTJ.jpg";
+import KVSuavitel_MKTJ from "../img/MKT  JOURNAL/Suavitel/KV Suavitel_ MKTJ.jpg";
+
 
 import error404 from "../img/404.svg";
 import cable from "../img/conect.svg";
+
+// Colgate total 12
+import Total12_Principal from "../img/MKT  JOURNAL/Total12/KV Total12 _ MKTJ.jpg";
+// import Total12_PVenta from "../img/MKT  JOURNAL/KV Total12 _ MKTJ.jpg";
 
 export const Images =
 {
@@ -36,9 +40,78 @@ export const Images =
     "KVPtx_MKTJ": KVPtx_MKTJ,
     "KVSS_MKTJ": KVSS_MKTJ,
     "KVSuavitel_MKTJ": KVSuavitel_MKTJ,
-    "KVTotal12_MKTJ": KVTotal12_MKTJ,
 
     "error404": error404,
     "cable": cable,
 
+    // Colgate total 12
+    "Total12_Principal": "https://i.ibb.co/d6hBbVx/KV-Total12-MKTJ.jpg",
+    "Total12_Minegocio": "https://i.ibb.co/Vp1XsTg/Total12-Minegocio.png",
+    "Total12_Puntoventa": "https://i.ibb.co/4F79gW3/Total12-Punto-de-venta.jpg",
+    "Total12_Poster": "https://i.ibb.co/3CGwh1g/Total12.jpg",
+    "Total12_Tapabocas": "https://i.ibb.co/S5DhLxM/Usa-el-tapabocas.jpg",
+
+    // Zig Zag
+    "Zigzag_Principal": "https://i.ibb.co/9r9yLFJ/KV-Zigzag-MKTJ.jpg",
+    "Zigzag_Poster": "https://i.ibb.co/mhh67Tw/ZigZag.jpg",
+    "Zigzag_Cenefa": "https://i.ibb.co/CbhPmL2/Zig-Zag-cenefa.jpg",
+    "Zigzag_Cenefa2": "https://i.ibb.co/YbvwQ0C/Zig-Zag-cenefa-2.jpg",
+
+    //CURCUMA
+    "Curcuma_PVenta1": "https://i.ibb.co/kSnr79L/C-rcuma-punto-venta.png",
+    "Curcuma_PVenta2": "https://i.ibb.co/vXWXWgd/C-rcuma-punto-venta2.png",
+    "Curcuma_PVenta3": "https://i.ibb.co/6Pk84x4/C-rcuma-punto-venta3.png",
+    "Curcuma_PVenta4": "https://i.ibb.co/R09QM2z/C-rcuma-punto-venta4.jpg",
+    "Curcuma_Principal": "https://i.ibb.co/1KpX48g/C-rcuma.jpg",
+
+    // COLGATE ZERO
+    "Zero_Principal":"https://i.ibb.co/H774nqf/Zero.jpg",
+    "Zero_PVenta1":"https://i.ibb.co/kHtqfrH/Zero-Punto-de-venta.jpg",
+
+
+    // PERSONAL CARE
+    // PROTEX
+    "JabonProtex":"https://i.ibb.co/fryTFm7/KV-Ptx-jab-n-MKTJ.jpg",
+    "ManosProtex":"https://i.ibb.co/JCdtcP5/KV-Ptx-MKTJ.jpg",
+    "Palmolive":"https://i.ibb.co/7YYR4Dc/KV-PO-MKTJ.jpg",
+    "Desodorante":"https://i.ibb.co/RhqbcmH/KV-SS-MKTJ.jpg",
+
+
 }
+/*
+AXION
+
+P - https://i.ibb.co/CBgB3y9/KV-Axion-MKTJ.jpg
+BANER - https://i.ibb.co/2jbvmvQ/Axion-cenefa.png
+JABON - https://i.ibb.co/fnrJskN/Axion.jpg
+AXION -https://i.ibb.co/zPd2Vns/Axion2.jpg
+
+CUIDAPLANETA
+
+PVENTA 1 - https://i.ibb.co/fnL7qF6/Home-care-Punto-de-venta.jpg
+PVENTA 2 - https://i.ibb.co/095TZ3S/Home-care-Punto-de-venta2.png
+P - https://i.ibb.co/XbG0Wfx/KV-HC-MKTJ.jpg
+PVENTA 3 - https://i.ibb.co/7vY9kzX/Zero-Punto-de-venta2.jpg
+
+CURCUMA
+PVENTA 1 - https://i.ibb.co/kSnr79L/C-rcuma-punto-venta.png
+PVENTA 2 - https://i.ibb.co/vXWXWgd/C-rcuma-punto-venta2.png
+PVENTA 3 - https://i.ibb.co/6Pk84x4/C-rcuma-punto-venta3.png
+PVENTA 4 - https://i.ibb.co/R09QM2z/C-rcuma-punto-venta4.jpg
+P - https://i.ibb.co/1KpX48g/C-rcuma.jpg
+
+TOTAL 12
+P - https://i.ibb.co/d6hBbVx/KV-Total12-MKTJ.jpg
+MI NEGOCIO - https://i.ibb.co/Vp1XsTg/Total12-Minegocio.png
+PVENTA - https://i.ibb.co/4F79gW3/Total12-Punto-de-venta.jpg
+POSTER - https://i.ibb.co/3CGwh1g/Total12.jpg
+TAPABOCAS - https://i.ibb.co/S5DhLxM/Usa-el-tapabocas.jpg
+
+ZIG-ZAG
+P - https://i.ibb.co/9r9yLFJ/KV-Zigzag-MKTJ.jpg
+PORTER - https://i.ibb.co/mhh67Tw/ZigZag.jpg
+CENEFA -https://i.ibb.co/CbhPmL2/Zig-Zag-cenefa.jpg
+
+
+
+*/
--- src/Constant/Marketing.js
+++ src/Constant/Marketing.js
@@ -1,6 +1,6 @@
 import { Images } from "./Images";
 let i = 0;
-// let j;
+let j = 1000;
 
 export const banner =[
     {
@@ -20,108 +20,227 @@ export const banner =[
 export const innovaciones_oral = [
     {
         _id: i++,
-        fecha:" Abril 2021",
-        titulo: "Lanzamiento Nuevo Régimen Encías Reforzadas",
+        fecha:" Julio 2021",
+        titulo: "OC - Crema Dental Total 12 Clean Mint 38ml",
         subtitulo: "Próximamente",
-        descripcion: "Conoce el nuevo Régimen para el cuidado de tus encías, con protección antibacterial. Úsalos juntos y obtén una salud bucal completa por 12 horas*",
-        imagenP: `${Images.KVZigzag_MKTJ}`,
+        descripcion: `Los beneficios que ofrece este lanzamiento son:
+        1. Protección Bucal Completa 
+        2. Disponibilidad en las tiendas de barrio/bodegas 
+        3. Nuevo Claim: Economía + Protección
+        `,
+        imagenP: `${Images.Total12_Principal}`,
+        publicidad: [
+            {
+                _idP: j++,
+                tipo: "v",
+                pTitulo: "TVC",
+                pSrc: `https://youtu.be/2O6olWmQ-aE`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Total12_Puntoventa}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Total12_Minegocio}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Total12_Poster}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Total12_Tapabocas}`,
+            }
+        ]
+    },
+    {
+        _id: i++,
+        fecha:"Junio 2021",
+        titulo: "Cepillo Dental Zig Zag Charcoal ",
+        subtitulo: "Próximamente",
+        descripcion: "El nuevo cepillo Zig Zag Charcoal cuenta con cerdas con infusión de carbón activado en forma de V que remueven las bacterias que causan el mal olor.",
+        imagenP: `${Images.Zigzag_Principal}`,
+        publicidad: [
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Zigzag_Poster}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta - Cenefa",
+                pSrc: `${Images.Zigzag_Cenefa}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta - Cenefa",
+                pSrc: `${Images.Zigzag_Cenefa2}`,
+            }
+        ]
+    },
+    {
+        _id: i++,
+        fecha:"Julio 2021",
+        titulo: "Crema Dental Colgate Natural Extracts Cúrcuma",
+        subtitulo: "Próximamente",
+        descripcion: "La fórmula de la Crema Dental Natural Extracts Limpieza Profunda, contiene extractos de cúrcuma que remueven las bacterias de tu boca, mientras la limpia profundamente dejando un sabor refrescante a yerbabuena.",
+        imagenP: `${Images.Curcuma_Principal}`,
+        publicidad: [
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Curcuma_PVenta1}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Curcuma_PVenta2}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Curcuma_PVenta3}`,
+            },
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Curcuma_PVenta4}`,
+            }
+        ]
+    },
+    {
+        _id: i++,
+        fecha:"Julio 2021",
+        titulo: "Crema Dental Colgate Zero",
+        subtitulo: "Próximamente",
+        descripcion: "La nueva Crema Dental Colgate Zero es 0% Artificial, siendo libre de Saborizantes, Endulzantes, Colorantes y Conservantes. Además, cuenta con una fórmula en Gel Transparente con sabor a menta y hierbabuena.",
+        imagenP: `${Images.Zero_Principal}`,
+        publicidad: [
+            {
+                _idP: j++,
+                tipo: "i",
+                pTitulo: "Punto de venta",
+                pSrc: `${Images.Zero_PVenta1}`,
+            }
+        ]
+    }
+];
+
+export const innovaciones_personal = [
+    {
+        _id: i++,
+        fecha:"Abril 2021",
+        titulo: "Protex Tamaño Familiar 1.3L",
+        subtitulo: "Próximamente",
+        descripcion: "",
+        imagenP: `${Images.JabonProtex}`,
         publicidad: [
             {
                 _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 1",
-                pSrc: `${Images.KVAxion_MKTJ}`,
-            },
-            {
-                _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 2",
-                pSrc: `${Images.KVHC_MKTJ}`,
-            },
-            {
-                _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 3",
-                pSrc: `${Images.KVPtx_MKTJ}`,
-            },
-            {
-                _idP: i++,
                 tipo: "v",
-                pTitulo: "Titulo 4",
+                pTitulo: "Digital",
                 pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
             }
         ]
     },
     {
         _id: i++,
-        fecha:"Mayo 2021",
-        titulo: "Lanzamiento Nuevo Régimen Encías Reforzadas",
+        fecha:"Julio 2021",
+        titulo: "El Poder de Las Manos",
         subtitulo: "Próximamente",
-        descripcion: "Conoce el nuevo Régimen para el cuidado de tus encías, con protección antibacterial. Úsalos juntos y obtén una salud bucal completa por 12 horas*",
-        imagenP: `${Images.KVPtxjabón_MKTJ}`,
+        descripcion: "",
+        imagenP: `${Images.ManosProtex}`,
         publicidad: [
             {
                 _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 1",
-                pSrc: `${Images.KVTotal12_MKTJ}`,
+                tipo: "v",
+                pTitulo: "Digital",
+                pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
             },
             {
                 _idP: i++,
                 tipo: "v",
-                pTitulo: "Titulo 2",
-                pSrc: "https://www.youtube.com/embed/qhfFpl0UQr0",
-            },
-            {
-                _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 3",
-                pSrc: `${Images.KVSuavitel_MKTJ}`,
-            },
-            {
-                _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 4",
-                pSrc: `${Images.KVSS_MKTJ}`,
-            }
-        ]
-    }
-
-];
-
-export const innovaciones_personal = [
-    {
-        _id: i++,
-        fecha:" Abril 2021",
-        titulo: "Lanzamiento Nuevo Régimen Encías Reforzadas",
-        subtitulo: "Próximamente",
-        descripcion: "Conoce el nuevo Régimen para el cuidado de tus encías, con protección antibacterial. Úsalos juntos y obtén una salud bucal completa por 12 horas*",
-        imagenP: `${Images.KVZigzag_MKTJ}`,
-        publicidad: [
-            {
-                _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 1",
-                pSrc: `${Images.KVAxion_MKTJ}`,
-            },
-            {
-                _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 2",
-                pSrc: `${Images.KVHC_MKTJ}`,
-            },
-            {
-                _idP: i++,
-                tipo: "i",
-                pTitulo: "Titulo 3",
-                pSrc: `${Images.KVPtx_MKTJ}`,
+                pTitulo: "Digital",
+                pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
             },
             {
                 _idP: i++,
                 tipo: "v",
-                pTitulo: "Titulo 4",
+                pTitulo: "Digital",
                 pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
             }
+        ]
+    },
+    {
+        _id: i++,
+        fecha:"Enero 2021",
+        titulo: "Relanzamiento de Palmolive",
+        subtitulo: "Próximamente",
+        descripcion: "",
+        imagenP: `${Images.Palmolive}`,
+        publicidad: [
+            {
+                _idP: i++,
+                tipo: "v",
+                pTitulo: "TVC",
+                pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
+            },
+            {
+                _idP: i++,
+                tipo: "v",
+                pTitulo: "Menciones",
+                pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
+            },
+            {
+                _idP: i++,
+                tipo: "v",
+                pTitulo: "Menciones",
+                pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
+            }
+        ]
+    },
+    {
+        _id: i++,
+        fecha:"Enero 2021",
+        titulo: "Lanzamiento Speed Stick Xtreme Night Gel 70 gr y Lady Speed Stick Talc en Crema 70 gr",
+        subtitulo: "Próximamente",
+        descripcion: "",
+        imagenP: `${Images.Desodorante}`,
+        publicidad: [
+            {
+                _idP: i++,
+                tipo: "v",
+                pTitulo: "TVC",
+                pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
+            },
+            // {
+            //     _idP: i++,
+            //     tipo: "v",
+            //     pTitulo: "Menciones",
+            //     pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
+            // },
+            // {
+            //     _idP: i++,
+            //     tipo: "v",
+            //     pTitulo: "Menciones",
+            //     pSrc: 'https://www.youtube.com/embed/qhfFpl0UQr0',
+            // }
         ]
     },
 ];
