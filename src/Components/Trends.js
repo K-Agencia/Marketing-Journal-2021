0caef774583fcce6a138cf8fa58231a913f6b2e9
Daniel Cosme <pipecosme123@gmail.com>
2021-07-07 13:33:02 -0500

    Se añadio la nueva animación para Trends

--- src/Components/Trends.js
+++ src/Components/Trends.js
@@ -5,6 +5,7 @@ import '../css/Trends.css';
 const Trends = () => {
 
     const container = useRef(null);
+    const titulo = useRef(null);
 
     useEffect(() => {
         lottie.loadAnimation({
@@ -16,12 +17,27 @@ const Trends = () => {
         });
     }, [])
 
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: titulo.current, // the dom element that will contain the animation
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../img/trendstitulo_colgateready.json') // the path to the animation json ../img/Animacion2.json  ----------- d6d6d6
+        });
+    }, [])
+
     return (
         <div className="Body_Trends">
-            <div className="container" ref={container}></div>
-            <div className="buttonsTrends">
-                <button>Media</button>
-                <button>Insights</button>
+            <div className="container_trends_titulo" ref={titulo}></div>
+            <div className="Body_Trends_animacion">
+                <div className="buttonsTrends">
+                    <button className="boton1">Insights</button>
+                </div>
+                <div className="container_trends" ref={container}></div>
+                <div className="buttonsTrends">
+                    <button className="boton2">Media</button>
+                </div>
             </div>
         </div>
     );
